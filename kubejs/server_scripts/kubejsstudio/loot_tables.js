01612f644c7f3ebb9124d392c9f9f301572b4599
JayceTheGaymer <150985485+JayceTheGaymer@users.noreply.github.com>
2026-08-29 19:55:59 -0400

    Created script to disable Deeper Darker additional warden drop injects, instead putting them directly into the normal loot table with KubeJS. This is to fix an issue with them not displaying in JEI.

--- kubejs/server_scripts/kubejsstudio/loot_tables.js
+++ kubejs/server_scripts/kubejsstudio/loot_tables.js
@@ -1,4 +1,4 @@
 // kubejsstudio loot tables; re-apply merges by table id
 ServerEvents.generateData('after_mods', event => {
-    event.json('minecraft:loot_table/entities/warden', {"type":"minecraft:entity","random_sequence":"minecraft:entities/warden","pools":[{"rolls":1.0,"bonus_rolls":0.0,"name":"pool0","entries":[]},{"rolls":1.0,"bonus_rolls":0.0,"name":"pool1","entries":[]}]})
+    event.json('minecraft:loot_table/entities/warden', {"type":"minecraft:entity","random_sequence":"minecraft:entities/warden","pools":[{"rolls":1.0,"bonus_rolls":0.0,"entries":[{"type":"minecraft:item","name":"minecraft:sculk_catalyst"}]},{"rolls":1,"bonus_rolls":0,"entries":[{"type":"minecraft:item","name":"deeperdarker:warden_carapace","functions":[{"function":"minecraft:set_count","count":{"type":"minecraft:uniform","min":1,"max":3}}]}]},{"rolls":1,"bonus_rolls":0,"entries":[{"type":"minecraft:item","name":"deeperdarker:heart_of_the_deep"}]}]})
 })
